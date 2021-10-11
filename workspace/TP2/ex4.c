6adb6635ca773978d15e15db7e8333fb8e0e7d1a
Atef <58571715+IMagwaI@users.noreply.github.com>
2021-10-11 15:51:00 +0200

    TP2- Ex4 partie 1

---
+++ workspace/TP2/ex4.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+enum Mois
+{
+    jan = 1,
+    fev,
+    mars,
+    avr,
+    mai,
+    juin,
+    juil,
+    aout,
+    sept,
+    oct,
+    nov,
+    dec
+};
+struct Date
+{
+    int jour;
+    enum Mois mois;
+    int annee;
+};
+typedef enum Mois Mois;
+typedef struct Date Date;
+void afficheDate(Date *d)
+{
+    char *Mois;
+    switch (d->mois)
+    {
+    case 1:
+        Mois = "Janvier";
+        break;
+
+    case 2:
+        Mois = "Février";
+        break;
+    case 3:
+        Mois = "Mars";
+        break;
+    case 4:
+        Mois = "Avril";
+        break;
+    case 5:
+        Mois = "Mai";
+        break;
+    case 6:
+          Mois = "Juin";
+        break;
+    case 7:
+        Mois = "Juillet";
+        break;
+    case 8:
+        Mois = "Aout";
+        break;
+    case 9:
+        Mois = "Septembre";
+        break;
+    case 10:
+        Mois = "Octobre";
+        break;
+    case 11:
+        Mois = "Novembre";
+        break;
+    case 12:
+        Mois = "Décembre";
+        break;
+
+    default:
+        break;
+    }
+}
+
+void initialiseDate(Date *d)
+{
+
+    printf("Saissir jour: ");
+    scanf("%i", &d->jour);
+    // d -> equiv  (*d).jour
+    printf("Saissir mois: ");
+    scanf("%i", (int *)&d->mois);
+    printf("Saissir année: ");
+    scanf("%i", &d->annee);
+}
+int main(void)
+{
+    Date d;
+    initialiseDate(&d);
+    afficheDate(&d);
+}
